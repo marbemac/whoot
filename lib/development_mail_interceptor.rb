5ae8cd1527671de569babc67df415b9ff2bb2ae8
Matt McCormick <matt.c.mccormick@gmail.com>
2011-12-20 18:27:52 -0500

    Updated welcome and invite emails, added Matt to email interceptors
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "marbemac@gmail.com"
+    message.to = "marbemac@gmail.com, matt.c.mccormick@gmail.com"
   end
 end
--- lib/staging_mail_interceptor.rb
+++ lib/staging_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class StagingMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "marbemac@gmail.com, wquartner@gmail.com, rpc37@cornell.edu"
+    message.to = "marbemac@gmail.com, wquartner@gmail.com, rpc37@cornell.edu, matt.c.mccormick@gmail.com"
   end
 end
