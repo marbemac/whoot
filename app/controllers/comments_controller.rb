2e71d41ba1463fb1b666f2e270bb532317dab138
Marc MacLeod <marbemac@gmail.com>
2012-01-03 16:48:46 -0500

    comments api update
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,11 +20,15 @@ class CommentsController < ApplicationController
         })
         Notification.add(user, 'comment', (user.settings.email_comment ? true : false), true, false, current_user, [Chronic.parse('today at 12:01am'), Chronic.parse('today at 11:59pm')], nil)
 
-        html = render_to_string :partial => 'teaser', :locals => {:comment => @comment}
-        content = {:status => 'OK', :comment => html, :root_id => post.id, :user_id => post.user_snippet.id, :event => 'comment_created' }
-        render json: content, status: :created
+        if params[:format] == :api
+          render :json => {:status => 'ok', :data => nil}, :status => :created
+        else
+          html = render_to_string :partial => 'teaser', :locals => {:comment => @comment}
+          content = {:status => 'ok', :comment => html, :root_id => post.id, :user_id => post.user_snippet.id, :event => 'comment_created' }
+          render json: content, status: :created
+        end
       else
-        render json: {:status => :ok, :errors => @comment.errors}, status: :unprocessable_entity
+        render json: {:status => 'error', :errors => @comment.errors}, status: :unprocessable_entity
       end
     end
   end
@@ -33,7 +37,7 @@ class CommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = post.comments.detect{|c| c.id.to_s == params[:comment_id]}
     html = render_to_string :partial => 'teaser', :locals => {:comment => comment}
-    response = {:status => 'OK', :comment => html }
+    response = {:status => 'ok', :comment => html }
     render json: response, status: 200
   end
 
