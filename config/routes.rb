0800b5c7623625bcc154122c52d2304c1605832c
Marc MacLeod <marbemac@gmail.com>
2012-01-03 16:56:57 -0500

    fix following
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -79,7 +79,7 @@ Whoot::Application.routes.draw do
       post 'ping' => 'pings#create', :defaults => { :format => :api }
       get 'undecided' => 'api#undecided', :defaults => { :format => :api }
       get 'users/me' => 'api#me', :defaults => { :format => :api }
-      get 'users/:id/following' => 'users#following', :defaults => { :format => :api }
+      get 'users/:id/following' => 'users#following_users', :defaults => { :format => :api }
       get 'users/:id/followers' => 'users#followers', :defaults => { :format => :api }
       get 'users/:id' => 'users#show', :defaults => { :format => :api }
     end
