090655dbd85ca76711266eb7b983f75dae488eac
Matt McCormick <matt.c.mccormick@gmail.com>
2012-02-13 15:05:10 -0500

    redirect to invites on first sign-in
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
+      if @user.sign_in_count == 0
+        redirect_to invites_path
+      else
+        sign_in_and_redirect @user, :event => :authentication
+      end
     else
       session["devise.facebook_data"] = env["omniauth.auth"].except('extra')
       redirect_to new_user_registration_url
