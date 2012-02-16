7d0705242a03e61297aa6a5ce46095292e7728c6
Matt McCormick <matt.c.mccormick@gmail.com>
2012-02-15 20:35:17 -0500

    1ppn views
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- app/models/post_event.rb
+++ app/models/post_event.rb
@@ -3,4 +3,14 @@ class PostEvent
   include Mongoid::Timestamps::Created
 
   embedded_in :post
+
+  def comment?
+    self._type == "PostCommentEvent"
+  end
+  def loop?
+    self._type == "PostLoopEvent"
+  end
+  def change?
+    self._type == "PostChangeEvent"
+  end
 end
