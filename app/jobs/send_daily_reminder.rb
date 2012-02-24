3c09c7c9f784a22a650a305ebca9a47e66ac5440
Matt McCormick <matt.c.mccormick@gmail.com>
2012-02-24 18:45:25 -0500

    stats, no daily email
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- app/jobs/send_daily_reminder.rb
+++ app/jobs/send_daily_reminder.rb
@@ -3,12 +3,8 @@ class SendDailyReminder
 
   def self.perform
     User.all().each do |user|
-      if !user.posted_today? && user.settings.email_daily
-        if user.device_token
-          Notification.send_push_notification(user.device_token, user.device_type, "Create a post for tonight and see what your friends are up to!")
-        else
-          UserMailer.daily_email(user).deliver
-        end
+      if !user.posted_today? && user.device_token && user.settings.email_daily
+        Notification.send_push_notification(user.device_token, user.device_type, "Create a post for tonight and see what your friends are up to!")
       end
     end
   end
--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -4,7 +4,7 @@ class UserSettings
   field :email_comment, :default => true
   field :email_ping, :default => true
   field :email_follow, :default => true
-  field :email_daily, :default => true
+  field :email_daily, :default => true # determines whether app users get daily push notifications
 
   embedded_in :user
 
