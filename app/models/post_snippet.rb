7d27801e38e420564257bb09765237f5047ec876
Marc MacLeod <marbemac@gmail.com>
2012-03-20 14:04:48 -0400

    tag
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/models/post_snippet.rb
+++ app/models/post_snippet.rb
@@ -4,11 +4,11 @@ class PostSnippet
   field :night_type
   field :created_at, :type => Time
   field :comment_id
-  field :tag
 
   embedded_in :post_assignable, polymorphic: true
 
   embeds_one :venue, :as => :has_venue, :class_name => 'VenueSnippet'
+  embeds_one :tag, :as => :taggable, :class_name => 'TagSnippet'
 
   class << self
     def conver_for_api(snippet)
