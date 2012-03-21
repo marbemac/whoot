2650e3e6996ddb8d1740569357a21de6c534fa82
Marc MacLeod <marbemac@gmail.com>
2012-03-20 20:31:00 -0400

    fix
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,3 @@
 Airbrake.configure do |config|
-  if Rails.env.staging?
-    config.api_key = '1236b076f831c4df73ba75709c1a875b'
-  else
-    config.api_key = 'ff9be33c7b8b0167ef0a7a37458daae1'
-  end
+  config.api_key = 'ff9be33c7b8b0167ef0a7a37458daae1'
 end
