ba9f6eb4622ca4e549026a672b127dd49234aa99
Marc MacLeod <marbemac@gmail.com>
2012-03-27 20:33:44 -0400

    torquebox files
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,13 +35,13 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.production.conf /etc/nginx/sites-enabled/#{application}"
     # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/torquebox.production.yml"), "#{shared_path}/config/torquebox.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, :roles => :app do
-    # run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/torquebox.yml #{release_path}/config/torquebox.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
