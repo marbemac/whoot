5519f80165d5b11c8e672140674260cff432c524
Matt McCormick <matt.c.mccormick@gmail.com>
2012-06-26 01:11:48 -0400

    json fix for night_invite
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -176,11 +176,12 @@ class ApiUsersController < ApplicationController
   end
 
   def night_invite
-    ActionMailer::Base.mail(:from => 'support@thewhoot.com', :to => 'matt@thewhoot.com', :subject => "night_invite params", :body => params.to_json).deliver
-    if params[:phone_numbers].blank? && params[:user_ids].blank?
+    #ActionMailer::Base.mail(:from => 'support@thewhoot.com', :to => 'matt@thewhoot.com', :subject => "night_invite params", :body => params.to_json).deliver
+    object = ActiveSupport::JSON.decode(request.body.read)
+    if object["phone_numbers"].blank? && object["user_ids"].blank?
       render json: build_ajax_response(:error, nil, "Both arrays cannot be blank"), status: 422
     else
-      night_invite = NightInvite.create(:phone_numbers => params[:phone_numbers], :invited_user_ids => params[:user_ids])
+      night_invite = NightInvite.create(:phone_numbers => object["phone_numbers"], :invited_user_ids => object["user_ids"])
       render json: build_ajax_response(:ok, nil, "Invite successful"), status: 201
     end
   end
